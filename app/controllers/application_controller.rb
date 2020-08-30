9066fee5242e6345619776bb1a3ffd5f76188a0a
Dannie Magalhaes <seliksy@gmail.com>
2020-08-30 20:45:37 -0300

    Remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
+  include TokenManager
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
-  end
 end
