0d195475814c9810ad75d2e293365ffd60fb8f73
Dannie Magalhaes <seliksy@gmail.com>
2020-08-29 22:09:22 -0300

    Move method to keep DRY principle

--- app/controllers/custom_devise/sessions_controller.rb
+++ app/controllers/custom_devise/sessions_controller.rb
@@ -1,4 +1,6 @@
 class CustomDevise::SessionsController < Devise::SessionsController
+  include DeviseManager
+
   after_action :authenticate_zype_user
 
   def new
@@ -7,22 +9,5 @@ class CustomDevise::SessionsController < Devise::SessionsController
 
   def create
     super
-  end
-
-  private
-
-  def authenticate_zype_user
-    return unless current_user
-    begin
-      if current_user.token_expires_at.nil? || current_user.token_expires_at <= Time.now
-        client = OAuth2::Client.new(Rails.application.credentials[:client_id],
-                                    Rails.application.credentials[:client_secret],
-                                    site: Rails.application.credentials[:provider_site],
-                                    token_url: Rails.application.credentials[:provider_token_url])
-        token = client.password.get_token(params[:user][:password], params[:user][:password])
-      end
-    rescue OAuth2::Error => e
-      current_user.update_attributes(token_expires_at: nil)
-    end
   end
 end
