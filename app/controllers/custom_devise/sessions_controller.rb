f4bf59fdcd156d98d3082f704062c7b507170f5d
Dannie Magalhaes <seliksy@gmail.com>
2020-08-29 22:00:32 -0300

    Nullify expire date if user does not exist

--- app/controllers/custom_devise/sessions_controller.rb
+++ app/controllers/custom_devise/sessions_controller.rb
@@ -14,12 +14,15 @@ class CustomDevise::SessionsController < Devise::SessionsController
   def authenticate_zype_user
     return unless current_user
     begin
-      client = OAuth2::Client.new(Rails.application.credentials[:client_id],
-                                  Rails.application.credentials[:client_secret],
-                                  site: Rails.application.credentials[:provider_site],
-                                  token_url: Rails.application.credentials[:provider_token_url])
-      token = client.password.get_token(params[:user][:password], params[:user][:password])
+      if current_user.token_expires_at.nil? || current_user.token_expires_at <= Time.now
+        client = OAuth2::Client.new(Rails.application.credentials[:client_id],
+                                    Rails.application.credentials[:client_secret],
+                                    site: Rails.application.credentials[:provider_site],
+                                    token_url: Rails.application.credentials[:provider_token_url])
+        token = client.password.get_token(params[:user][:password], params[:user][:password])
+      end
     rescue OAuth2::Error => e
+      current_user.update_attributes(token_expires_at: nil)
     end
   end
 end
