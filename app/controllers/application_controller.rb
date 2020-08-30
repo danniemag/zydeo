13b6d7bbfe6bb8565d45a5817bbf6200b692bf4b
Dannie Magalhaes <seliksy@gmail.com>
2020-08-29 21:05:43 -0300

    Remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    session[:my_account] = current_user
     stored_location_for(resource) || root_path
   end
 end
