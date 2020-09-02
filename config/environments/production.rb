d98cd1b3c08bc5e6e6b0132504028cedb08e63d2
Dannie Magalhaes <seliksy@gmail.com>
2020-09-01 21:57:11 -0300

    Update production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -64,9 +64,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "zydeo_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  #TODO: set actual application's host
-  config.action_mailer.default_url_options = { host: '', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
