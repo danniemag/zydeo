b8dedc15ced009cd3b6900c2e9ddeb1631fcd75f
Dannie Magalhaes <seliksy@gmail.com>
2020-09-02 10:21:19 -0300

    Remove unused code

--- app/scopes/user_search.rb
+++
@@ -1,9 +0,0 @@
-require 'active_support/concern'
-
-module UserSearch
-  extend ActiveSupport::Concern
-
-  def is_customer
-    self.token_expires_at.present? && self.token_expires_at >= Time.now
-  end
-end
